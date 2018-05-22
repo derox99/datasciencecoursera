d45f5957bb5210d9c93ec03ee6eddd47569daa03
alederos <alessio.derossi@eng.it>
2018-05-22 20:18:19 +0200

    added graph 4 and 5

--- exploratory_data_analysis/plots.R
+++ exploratory_data_analysis/plots.R
@@ -1,4 +1,27 @@
 library(dplyr)
+library(ggplot2)
 balt<-NEI[NEI$fips=="24510",]
 baltGrouped<-balt %>% group_by(year,type) %>% summarise(Emissions=sum(Emissions))
 qplot(x = year, y=Emissions, data=baltGrouped, color=type, geom = "line")
+
+
+
+indexComb<-grep("Comb",SCC$EI.Sector)
+indexCoal<-grep("Coal",SCC$EI.Sector)
+indexCoalComb<-intersect(indexCoal,indexComb)
+sccCoalComb<-as.character(SCC[indexCoalComb,"SCC"])
+NEICoal<-NEI[NEI$SCC %in% sccCoalComb,]
+coalUSA<-NEICoal %>% group_by(year) %>% summarise(Emissions=sum(Emissions))
+qplot(x = year, y=Emissions, data=coalUSA, geom = "line")
+
+
+indexVehicle<-grep("Vehicle",SCC$EI.Sector)
+sccVehicle<-as.character(SCC[indexVehicle,"SCC"])
+
+balt<-NEI[NEI$fips=="24510",]
+#merge data with SCC
+baltSCC<-merge(x = balt, y = SCC, by.x = "SCC", by.y = "SCC")
+#find vehicle in level two
+baltVehicle<-baltSCC[grep("Vehicle",baltSCC$SCC.Level.Two),]
+baltVehicleGrouped<-baltVehicle %>% group_by(year,SCC.Level.Two) %>% summarise(Emissions=sum(Emissions))
+qplot(x = year, y=Emissions, data=baltVehicleGrouped, color=SCC.Level.Two, geom = "line")
